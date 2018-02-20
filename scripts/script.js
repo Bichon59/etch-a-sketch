73d85ddcb78bc49abcc0828466fc68f6748a0bb6
Bichon5960 <morin.nicolas.erwan@gmail.com>
2018-02-20 10:29:16 +0100

    Modification du vendredi 16 fevrier

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,11 @@ const container = document.querySelector('#container');
 
 function createGrid(container, xSize, ySize, boxSize) {
 	if (xSize%boxSize !== 0 || ySize%boxSize !== 0) {
-		return 'Invalid parameters';
+		xSize = Math.floor(xSize);
+		ySize = Math.floor(ySize);
 	}
 
-	container.style.cssText = 'grid-template : repeat(' + xSize/boxSize + ', ' + boxSize + 'px) / repeat(' + ySize/boxSize + ', ' + boxSize + 'px)';
+	container.style.cssText = 'grid-template : repeat(' + ySize/boxSize + ', ' + boxSize + 'px) / repeat(' + xSize/boxSize + ', ' + boxSize + 'px)';
 
 	for(let i = 0; i < xSize/boxSize; i++) {
 		for(let j = 0; j < ySize/boxSize; j++) {
@@ -19,11 +20,24 @@ function createGrid(container, xSize, ySize, boxSize) {
 }
 
 function color(e) {
-	console.log(e.target.style.color);
 	e.target.style.backgroundColor = 'red';
 }
 
-createGrid(container, 800, 800, 16);
-const boxes = document.querySelectorAll('.box');
 
+function clearGrid(e) {
+	console.log(e);
+	choice = prompt("How many elements per sides ?");
+
+	if (isNaN(choice)) choice = 50;
+
+	createGrid(container, 800, 800, 800/choice);
+}
+
+createGrid(container, 800, 800, 16);
+
+const boxes = document.querySelectorAll('.box');
 boxes.forEach(box => box.addEventListener('mouseover', color));
+
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clearGrid);
